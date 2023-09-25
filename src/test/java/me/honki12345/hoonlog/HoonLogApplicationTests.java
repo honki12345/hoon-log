5c83c62af9354a855be901a28c4ec3e14d6cbe63
honki12345 <iui4793@gmail.com>
2023-09-25 20:08:56 +0900

    #1 - 프로젝트 생성
    `Spring Initializr` 을 이용하여 다음의 의존성을 추가한 후 프로젝트를 생성하였다
    
    * jpa
    * security
    * spring web
    * lombok
    * dev tools
    * h2
    * mysql driver

---
+++ src/test/java/me/honki12345/hoonlog/HoonLogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.honki12345.hoonlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
