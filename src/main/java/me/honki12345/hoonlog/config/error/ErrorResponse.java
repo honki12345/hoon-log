cede5e7653ef71b97df2275924c74d3546a11d5f
honki12345 <iui4793@gmail.com>
2023-09-26 21:35:38 +0900

    #4 - 에러코드 & 에러메시지 객체 설계
    에러 코드를 한 곳에 모아 관리하기 위한 ErrorCode enum 객체와
    에러 발생시 HTTP response 포맷을 위한
    ErrorResponse 객체를 설계하였다.

---
+++ src/main/java/me/honki12345/hoonlog/config/error/ErrorResponse.java
@@ -0,0 +1,14 @@
+package me.honki12345.hoonlog.config.error;
+
+public record ErrorResponse(
+        String message,
+        String code
+) {
+    public static ErrorResponse of(ErrorCode code) {
+        return new ErrorResponse(code.getMessage(), code.getCode());
+    }
+
+    public static ErrorResponse of(ErrorCode code, String message) {
+        return new ErrorResponse(message, code.getCode());
+    }
+}
