f0bcbaf5478f8529a26e1777046147e4e1d9a871
honki12345 <iui4793@gmail.com>
2023-10-01 17:07:25 +0900

    #5 - security logging 설정: securityConfig -> application.yaml

--- src/main/java/me/honki12345/hoonlog/config/SecurityConfig.java
+++ src/main/java/me/honki12345/hoonlog/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
