fb9eca2995b6653f486c681abc5244906a74cc0e
honki12345 <iui4793@gmail.com>
2023-10-02 16:40:36 +0900

    #6 - 생성/수정자, 일자 자동생성을 위한 Auditing 구현

--- src/main/java/me/honki12345/hoonlog/config/JpaConfig.java
+++ src/main/java/me/honki12345/hoonlog/config/JpaConfig.java
@@ -1,10 +1,26 @@
 package me.honki12345.hoonlog.config;
 
+import java.util.Optional;
+import me.honki12345.hoonlog.security.jwt.token.JwtAuthenticationToken;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+            .map(JwtAuthenticationToken.class::cast)
+            .map(JwtAuthenticationToken::getName);
+    }
 }
