ac09850341a6a153449432890fdd89249ad81092
honki12345 <iui4793@gmail.com>
2023-10-02 15:27:13 +0900

    #5 - Authentication 객체 파라미터 주입을 위한 ArgumentResolver 구현

---
+++ src/main/java/me/honki12345/hoonlog/security/jwt/util/IfLoginArgumentResolver.java
@@ -0,0 +1,58 @@
+package me.honki12345.hoonlog.security.jwt.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+import me.honki12345.hoonlog.dto.security.LoginInfoDTO;
+import me.honki12345.hoonlog.dto.security.LoginUserDTO;
+import me.honki12345.hoonlog.security.jwt.token.JwtAuthenticationToken;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class IfLoginArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(IfLogin.class) != null
+            && parameter.getParameterType() == LoginUserDTO.class;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = null;
+        try {
+            authentication = SecurityContextHolder.getContext().getAuthentication();
+        } catch (Exception e) {
+            return null;
+        }
+
+        if (authentication == null) {
+            return null;
+        }
+
+        JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) authentication;
+        Object principal = authenticationToken.getPrincipal();
+
+        if (principal == null) {
+            return null;
+        }
+        LoginInfoDTO loginInfoDTO = (LoginInfoDTO) principal;
+        LoginUserDTO loginUserDTO = LoginUserDTO.of(loginInfoDTO.userId(), loginInfoDTO.username());
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        while (iterator.hasNext()) {
+            GrantedAuthority grantedAuthority = iterator.next();
+            String role = grantedAuthority.getAuthority();
+            loginUserDTO.addRole(role);
+        }
+
+        return loginUserDTO;
+    }
+}
