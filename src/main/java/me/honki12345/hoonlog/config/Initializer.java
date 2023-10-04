e9afcda35fe220068271b1b5531a650b53a4377f
honki12345 <iui4793@gmail.com>
2023-10-04 23:03:40 +0900

    #8 - 로컬에 저장하기 위한 설정: 저장폴더 생성과 경로설정

--- src/main/java/me/honki12345/hoonlog/config/Initializer.java
+++ src/main/java/me/honki12345/hoonlog/config/Initializer.java
@@ -1,5 +1,8 @@
 package me.honki12345.hoonlog.config;
 
+import static me.honki12345.hoonlog.config.WebConfig.UPLOAD_LOCATION;
+
+import java.io.File;
 import me.honki12345.hoonlog.domain.Role;
 import me.honki12345.hoonlog.repository.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -7,16 +10,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RoleInitializer {
+public class Initializer {
+
+
 
     @Bean
-    public CommandLineRunner initRole(RoleRepository roleRepository) {
+    public CommandLineRunner init(RoleRepository roleRepository) {
         return args -> {
             if (roleRepository.count() == 0) {
                 Role userRole = Role.of("ROLE_USER");
 
                 roleRepository.save(userRole);
 
+            }
+
+            File folder = new File(UPLOAD_LOCATION);
+            if (!folder.exists()) {
+                folder.mkdir();
             }
         };
     }
--- src/main/java/me/honki12345/hoonlog/config/WebConfig.java
+++ src/main/java/me/honki12345/hoonlog/config/WebConfig.java
@@ -1,16 +1,31 @@
 package me.honki12345.hoonlog.config;
 
+import java.io.File;
 import java.util.List;
 import me.honki12345.hoonlog.security.jwt.util.IfLoginArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    public static final String UPLOAD_LOCATION =
+        System.getProperty("user.home") + File.separator + "hoonlog";
+    public static final String UPLOAD_URL = "/images/post";
+    public static final String IMAGE_LOCATION =
+        System.getProperty("user.home") + File.separator + "hoonlog"
+            + File.separator + "post";
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new IfLoginArgumentResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:///" + UPLOAD_LOCATION);
     }
 }
