9f84ab93ed973600f69a89fa15b34d9dab2958d3
honki12345 <iui4793@gmail.com>
2023-10-08 20:56:27 +0900

    #27 - 테스트시 파일저장 위치설정과 완료 후 파일삭제 로직구현

---
+++ src/test/java/me/honki12345/hoonlog/config/Initializer.java
@@ -0,0 +1,74 @@
+package me.honki12345.hoonlog.config;
+
+import static me.honki12345.hoonlog.domain.util.FileUtil.IMAGE_LOCATION;
+import static me.honki12345.hoonlog.domain.util.FileUtil.UPLOAD_LOCATION;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import java.io.File;
+import me.honki12345.hoonlog.domain.Role;
+import me.honki12345.hoonlog.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class Initializer {
+
+    @Bean
+    public CommandLineRunner init(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                Role userRole = Role.of("ROLE_USER");
+
+                roleRepository.save(userRole);
+
+            }
+
+            File uploadFolder = new File(UPLOAD_LOCATION);
+            if (!uploadFolder.exists()) {
+                uploadFolder.mkdir();
+            }
+
+            File imageFolder = new File(IMAGE_LOCATION);
+            if (!imageFolder.exists()) {
+                imageFolder.mkdir();
+            }
+        };
+    }
+
+    @Bean
+    ServletListenerRegistrationBean<ServletContextListener> servletListener() {
+        ServletListenerRegistrationBean<ServletContextListener> srb
+            = new ServletListenerRegistrationBean<>();
+        srb.setListener(new TestServletContextListener());
+        return srb;
+    }
+
+    static class TestServletContextListener implements ServletContextListener {
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            File imageFolder = new File(IMAGE_LOCATION);
+            if (imageFolder.exists()) {
+                deleteDir(imageFolder);
+                imageFolder.delete();
+            }
+
+            File uploadFolder = new File(UPLOAD_LOCATION);
+            if (uploadFolder.exists()) {
+                deleteDir(uploadFolder);
+                uploadFolder.delete();
+            }
+        }
+
+        private void deleteDir(File file) {
+            String[] entries = file.list();
+            for (String s : entries) {
+                File currentFile = new File(file.getPath(), s);
+                currentFile.delete();
+            }
+        }
+    }
+}
