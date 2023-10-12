3fcf701ea38eb00d9c266295e5d40cf7757e2f4f
honki12345 <iui4793@gmail.com>
2023-10-12 17:39:19 +0900

    #28 - 컨트롤러 계층에 swagger 설정

--- src/main/java/me/honki12345/hoonlog/controller/AuthController.java
+++ src/main/java/me/honki12345/hoonlog/controller/AuthController.java
@@ -1,11 +1,20 @@
 package me.honki12345.hoonlog.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.honki12345.hoonlog.dto.TokenDTO;
 import me.honki12345.hoonlog.dto.UserAccountDTO;
 import me.honki12345.hoonlog.dto.request.LoginRequest;
 import me.honki12345.hoonlog.dto.response.LoginResponse;
+import me.honki12345.hoonlog.dto.response.UserAccountResponse;
+import me.honki12345.hoonlog.error.ErrorResponse;
 import me.honki12345.hoonlog.service.AuthService;
 import me.honki12345.hoonlog.service.UserAccountService;
 import org.springframework.http.HttpStatus;
@@ -16,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "auth", description = "JWT API")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +34,16 @@ public class AuthController {
     private final AuthService authService;
     private final UserAccountService userAccountService;
 
+    @Operation(
+        summary = "로그인",
+        description = "아이디와 비밀번호를 받아 로그인을 합니다",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "로그인 성공", content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+            @ApiResponse(responseCode = "401",
+                description = "로그인에 실패하였습니다",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+        }
+    )
     @PostMapping("/token")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         UserAccountDTO userAccountDTO = userAccountService.findUserAccountAfterCheckingPassword(
@@ -34,12 +54,32 @@ public class AuthController {
         return new ResponseEntity<>(loginResponse, HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "로그아웃",
+        description = "로그인 상태의 유저의 토큰 데이터를 서버에서 삭제합니다",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+            @ApiResponse(responseCode = "400",
+                description = "유효하지 않은 액세스 토큰 값일 경우 로그아웃에 실패합니다",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+        }
+    )
     @DeleteMapping("/token")
     public ResponseEntity<Object> logout(@RequestBody TokenDTO tokenDTO) {
         authService.deleteRefreshToken(tokenDTO.refreshToken());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "액세스토큰 재발급",
+        description = "리프레쉬 토큰이 유효하면 액세스 토큰 재발급을 해줍니다",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "액세스토큰 재발급 성공"),
+            @ApiResponse(responseCode = "404",
+                description = "유효하지 않은 리프레쉬 토큰으로 요청시 재발급에 실패합니다",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+        }
+    )
     @PostMapping("/refreshToken")
     public ResponseEntity<LoginResponse> refreshToken(@RequestBody TokenDTO tokenDTO) {
         Long userId = authService.FindUserIdByRefreshToken(tokenDTO.refreshToken());
--- src/main/java/me/honki12345/hoonlog/controller/PostLikeController.java
+++ src/main/java/me/honki12345/hoonlog/controller/PostLikeController.java
@@ -1,6 +1,9 @@
 package me.honki12345.hoonlog.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.honki12345.hoonlog.dto.PostLikeDTO;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "like", description = "게시물좋아요 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts/like")
 @RestController
@@ -20,6 +24,13 @@ public class PostLikeController {
 
     private final PostLikeService postLikeService;
 
+    @Operation(
+        summary = "게시글 좋아요 생성",
+        description = "로그인한 유저는 게시글에 좋아요를 할 수 있습니다",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "게시글 좋아요 성공")
+        }
+    )
     @PostMapping
     public ResponseEntity<Object> create(
         @Valid @RequestBody PostLikeDTO postLikeDTO) {
@@ -27,6 +38,13 @@ public class PostLikeController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "게시글 좋아요 취소",
+        description = "로그인한 유저는 좋아요를 했던 게시글에 취소를 할 수 있습니다",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "게시글 좋아요 취소 성공")
+        }
+    )
     @DeleteMapping
     public ResponseEntity<Object> delete(
         @Valid @RequestBody PostLikeDTO postLikeDTO
