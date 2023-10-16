eedafdbbae0ee39fdcf942e3a64d364ab8b7f027
honki12345 <iui4793@gmail.com>
2023-10-16 16:25:02 +0900

    #36 - FileUtilsTest 의 SpringBootTest 의존성제거
    FileUtilsTest 에서 스프링 컨테이너 없어도
    인스턴스를 생성해서 테스트할 수 있어서 스프링 컨테이너 의존성을 삭제했다.

--- src/test/java/me/honki12345/hoonlog/domain/util/FileUtilsTest.java
+++ src/test/java/me/honki12345/hoonlog/domain/util/FileUtilsTest.java
@@ -5,27 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
 @ExtendWith(OutputCaptureExtension.class)
 @DisplayName("FileUtils 단위테스트")
-@SpringBootTest(classes = {FileUtils.class})
 class FileUtilsTest {
-
-    @Autowired
-    FileUtils fileUtils;
 
     @DisplayName("[삭제/실패]존재하지 않는 파일 삭제시, 로깅을 남긴다.")
     @Test
     void givenWrongFilePath_whenDeletingFile_thenLogging(CapturedOutput output) {
         // given // when
+        FileUtils fileUtils = new FileUtils();
         fileUtils.deleteFile("wrongFilePath");
 
         // then
         assertThat(output.getOut().contains("파일이 존재하지 않습니다: wrongFilePath")).isTrue();
     }
-
 }
