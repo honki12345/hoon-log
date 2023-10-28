e45f2fa5c6198ff4a48538932af7bf9d735a1289
honki12345 <iui4793@gmail.com>
2023-10-28 15:59:19 +0900

    #39 - elasticsearch client config

---
+++ src/main/java/me/honki12345/hoonlog/config/ElasticSearchConfig.java
@@ -0,0 +1,19 @@
+package me.honki12345.hoonlog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .build();
+    }
+}
