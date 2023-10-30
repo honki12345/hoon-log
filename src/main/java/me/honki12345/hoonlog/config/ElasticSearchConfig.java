8f6f74ffe020f0645167dbe868a8cfe013b6dc9f
honki12345 <iui4793@gmail.com>
2023-10-30 21:48:41 +0900

    #39 - ElasticSearchConfig 의 hostAndPost 값 주입과 Converter 추가
    ElasticSearchConfig 에서 elastic client 접속을 위하여 host 와 port 값을 입력하는 부분을
    하드코딩해서 적어주는 것에서 application.yaml 의 값을 주입하는 방식으로 변경하였다.
    
    또한 LocalDateTime 타입을 ElasitcSearch Repository 에 저장하고 꺼내어 객체로 만들 때
    Conversion 관련 오류를 없애기 위하여 Converter를 설정하였다.

--- src/main/java/me/honki12345/hoonlog/config/ElasticSearchConfig.java
+++ src/main/java/me/honki12345/hoonlog/config/ElasticSearchConfig.java
@@ -1,19 +1,60 @@
 package me.honki12345.hoonlog.config;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import me.honki12345.hoonlog.repository.elasticsearch.PostSearchRepository;
+import me.honki12345.hoonlog.repository.elasticsearch.UserAccountSearchRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@EnableElasticsearchRepositories(basePackageClasses = {PostSearchRepository.class,
+    UserAccountSearchRepository.class})
 @Configuration
-@EnableElasticsearchRepositories
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.hostAndPort}")
+    private String hostAndPost;
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(hostAndPost)
             .build();
+    }
+
+    @Bean
+    @Override
+    public ElasticsearchCustomConversions elasticsearchCustomConversions() {
+        return new ElasticsearchCustomConversions(
+            Arrays.asList(new LocalDateTimeToStringConverter(),
+                new StringToLocalDateTimeConverter())
+        );
+    }
+
+    @WritingConverter
+    static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
+
+        @Override
+        public String convert(LocalDateTime source) {
+            return source.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+    }
+
+    @ReadingConverter
+    static class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+
+        @Override
+        public LocalDateTime convert(String source) {
+            return LocalDateTime.parse(source, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
     }
 }
